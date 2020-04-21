e73f87d0e118f99eab88afbeeb32ad95f88b60e0
Khoi Le <43243135+Khoi8@users.noreply.github.com>
2020-04-21 19:53:08 -0400

    Deletes non int input

--- Counting.py
+++ Counting.py
@@ -2,8 +2,18 @@ async def double_count(message):
     # Check to see if message is in counting-channel
     if message.channel.name == 'test-buzz':
         print("Channel check works")
-        last_num = int(message.content)
-        await message.channel.send(last_num + 1)
+        if  is_number(message.content) : 
+            last_num = int(message.content)
+            await message.channel.send(last_num + 1)
+        else: 
+            await message.delete()
 
 def test():
     print('test')
+
+def is_number(s):
+    try:
+        int(s)
+        return True
+    except ValueError:
+        return False
