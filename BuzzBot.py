8e9be2f622accb800c63f068ff18a65046002f45
Khoi Le <khoi@Khois-MacBook-Pro.local>
2020-04-26 23:53:53 -0400

    cleaned it up a little bit

--- BuzzBot.py
+++ BuzzBot.py
@@ -5,12 +5,10 @@ import pymongo
 from pymongo import MongoClient
 
 cluster = MongoClient("mongodb+srv://buzzbot:neJPIzxDGax66NqI@buzzbot-gesgl.gcp.mongodb.net/test?retryWrites=true&w=majority")
-
 db = cluster["discord"]
 collection = db["chat logs"]
 
 token = json.load(open('auth.json', 'r'))['token']
-
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
@@ -18,7 +16,8 @@ class MyClient(discord.Client):
         #adds all the members to teh database on start of bot across all servers the bot is in
         x = self.get_all_members()
         for member in x:
-            post = {"_id": member.id, "name": member.name,"joined at": member.joined_at, "server": member.guild.name, "isBot": member.bot}
+            post = {"_id": member.id, "name": member.name,"joined at": member.joined_at, 
+            "server": member.guild.name, "isBot": member.bot}
             collection.insert_one(post)
 
     async def on_message(self, message):
