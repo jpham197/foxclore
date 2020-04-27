d660205642d55f0432bb64eddc19c6049a9e80e5
Khoi Le <khoi@Khois-MacBook-Pro.local>
2020-04-27 02:04:59 -0400

    Updated documentation and made it so that it addes new memeber to the database as they join

--- BuzzBot.py
+++ BuzzBot.py
@@ -4,23 +4,57 @@ import Counting as count
 import pymongo
 from pymongo import MongoClient
 
-cluster = MongoClient("mongodb+srv://buzzbot:neJPIzxDGax66NqI@buzzbot-gesgl.gcp.mongodb.net/test?retryWrites=true&w=majority")
+# Connecting to database and declaring the collection
+cluster = MongoClient("mongodb+srv://buzzbot:neJPIzxDGax66NqI@buzzbot-" 
++ "gesgl.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = cluster["discord"]
-collection = db["chat logs"]
+collection = db["users"]
 
 token = json.load(open('auth.json', 'r'))['token']
 class MyClient(discord.Client):
     async def on_ready(self):
+        """
+        Adds all users of servers that the bot is in to the database
+        :param self: this is our bot
+        """
+        # Shows that the login worked
         print('Logged on as', self.user)
-       
-        #adds all the members to teh database on start of bot across all servers the bot is in
+
+        # x is a list created by the method get_all_members()
         x = self.get_all_members()
         for member in x:
-            post = {"_id": member.id, "name": member.name,"joined at": member.joined_at, 
-            "server": member.guild.name, "isBot": member.bot}
+            post = {
+            "_id": member.id, 
+            "name": member.name,
+            "joined at": member.joined_at,
+            "server": member.guild.name, 
+            "isBot": member.bot
+            }
             collection.insert_one(post)
+    
+    
+    async def on_member_join(self,member):
+        """
+        Adds newly joined memebers while bot is on to the database
+        :param self: this is our bot
+        :param member: this is the memeber that has just joined
+        """
+        post = {
+            "_id": member.id, 
+            "name": member.name,
+            "joined at": member.joined_at,
+            "server": member.guild.name, 
+            "isBot": member.bot
+            }
+        collection.insert_one(post)
+
 
     async def on_message(self, message):
+        """
+        Adds newly joined memebers while bot is on to the database
+        :param self: this is our bot
+        :param message: this is any message that was sent in any server
+        """
         # don't respond to ourselves
         if message.author == self.user:
             return
@@ -28,7 +62,7 @@ class MyClient(discord.Client):
         if message.content == 'ping':
             await message.channel.send('pong')
 
-        await count.double_count(message)
+        await count.counting(message)
 
 client = MyClient()
 client.run(token)
--- Counting.py
+++ Counting.py
@@ -1,16 +1,23 @@
-async def double_count(message):
+async def counting(message):
+    """
+    manages the counting channel rules
+    :param message: this is any message that was sent in the counting channel
+    """
     # Check to see if message is in counting-channel
     if message.channel.name == 'test-buzz':
+        # Checks to see if message is an int
         if  is_number(message.content) : 
             last_num = int(message.content)
             await message.channel.send(last_num + 1)
         else: 
             await message.delete()
 
-def test():
-    print('test')
-
 def is_number(s):
+    """
+    Checks if the message can be an int
+    :param s: the message as a string
+    :returns: bool based on if the string to int conversion was successful
+    """
     try:
         int(s)
         return True
