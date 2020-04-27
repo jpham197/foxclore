0b66af0e658181aef7b6205b9a57fcab46d06d80
Jimmy <jpham197@gmail.com>
2020-04-27 16:20:37 -0400

    change last message checker to check last message in a channel rather than last message from buzz-bot

--- Counting.py
+++ Counting.py
@@ -1,3 +1,5 @@
+import random
+
 async def counting(message):
     """
     manages the counting channel rules
@@ -6,10 +8,10 @@ async def counting(message):
     # Check to see if message is in counting-channel
     if message.channel.name == 'test-buzz':
         if message.author.name != "buzz-bot":
-            m = await message.channel.history().get(author__name='buzz-bot')
-            # Checks to see if message is an int
+            last_message = await message.channel.history(limit=2).flatten()
+
             if is_number(message):
-                if in_order(message, m):
+                if in_order(message, last_message[1]):
                     await double_count(message)
                 else:
                     await message.delete()
@@ -19,17 +21,22 @@ async def counting(message):
 
 async def double_count(message):
     """
-    Make buzz-bot count one more time after user counts
+    Make buzz-bot count one more time after user counts, only happens sometimes
 
     :param message: the discord message the user sent
     :returns: no return value
     """
-    last_num = int(message.content)
-    await message.channel.send(last_num + 1)
+
+    chance = 50
+
+    if random.randint(1, 100) < chance:
+        last_num = int(message.content)
+        await message.channel.send(last_num + 1)
 
 def is_number(message):
     """
     Checks if the message can be an int
+
     :param s: the message as a string
     :returns: bool based on if the string to int conversion was successful
     """
@@ -40,6 +47,13 @@ def is_number(message):
         return False
 
 def in_order(message, last_message):
+    """
+    Checks if the message was correct
+
+    :param s: the message as a string
+    :returns: bool based on if the message is one greater than the last
+    """
+    print(f'Last Message is {last_message.content}')
     if int(message.content) == int(last_message.content) + 1:
         return True
     else:
