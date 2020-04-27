b358ad31b37d92b35b0525aed76d9e0dade68b8b
Khoi Le <khoi@Khois-MacBook-Pro.local>
2020-04-27 03:07:53 -0400

    Added user removal function and cleaned up code

--- BuzzBot.py
+++ BuzzBot.py
@@ -1,67 +1,28 @@
 import discord
 import json
 import Counting as count
-import pymongo
-from pymongo import MongoClient
+import updateUsers
 
-# Connecting to database and declaring the collection
-cluster = MongoClient("mongodb+srv://buzzbot:neJPIzxDGax66NqI@buzzbot-" 
-+ "gesgl.gcp.mongodb.net/test?retryWrites=true&w=majority")
-db = cluster["discord"]
-collection = db["users"]
-
+# auth token from auth.json
 token = json.load(open('auth.json', 'r'))['token']
-class MyClient(discord.Client):
-    async def on_ready(self):
-        """
-        Adds all users of servers that the bot is in to the database
-        :param self: this is our bot
-        """
-        # Shows that the login worked
-        print('Logged on as', self.user)
 
-        # x is a list created by the method get_all_members()
-        x = self.get_all_members()
-        for member in x:
-            post = {
-            "_id": member.id, 
-            "name": member.name,
-            "joined at": member.joined_at,
-            "server": member.guild.name, 
-            "isBot": member.bot
-            }
-            collection.insert_one(post)
-    
+
+class MyClient(discord.Client):
+    """ Our main discord bot class"""
+    async def on_ready(self):
+        """Stuff that happens when the bot is turned on"""
+        updateUsers.first_time(self)
     
     async def on_member_join(self,member):
-        """
-        Adds newly joined memebers while bot is on to the database
-        :param self: this is our bot
-        :param member: this is the memeber that has just joined
-        """
-        post = {
-            "_id": member.id, 
-            "name": member.name,
-            "joined at": member.joined_at,
-            "server": member.guild.name, 
-            "isBot": member.bot
-            }
-        collection.insert_one(post)
-
+        """Stuff that happens when there is a new member"""
+        updateUsers.new_member(member)
+    
+    async def on_member_remove(self, member):
+        """Stuff that happens when a member leaves"""
+        updateUsers.remove_member(member)
 
     async def on_message(self, message):
-        """
-        Adds newly joined memebers while bot is on to the database
-        :param self: this is our bot
-        :param message: this is any message that was sent in any server
-        """
-        # don't respond to ourselves
-        if message.author == self.user:
-            return
-
-        if message.content == 'ping':
-            await message.channel.send('pong')
-
+        """Stuff that happens when there is a new message"""
         await count.counting(message)
 
 client = MyClient()
---
+++ updateUsers.py
@@ -0,0 +1,53 @@
+import pymongo
+from pymongo import MongoClient
+
+# Connecting to database and declaring the collection
+cluster = MongoClient("mongodb+srv://buzzbot:neJPIzxDGax66NqI@buzzbot-" 
++ "gesgl.gcp.mongodb.net/test?retryWrites=true&w=majority")
+db = cluster["discord"]
+collection = db["users"]
+
+
+async def first_time(self):
+    """
+    Adds all users of servers that the bot is in to the database
+    :param self: this is our bot
+    """
+    # Shows that the login worked
+    print('Logged on as', self.user)
+
+    # x is a list created by the method get_all_members()
+    x = self.get_all_members()
+    for member in x:
+        post = {
+        "_id": member.id, 
+        "name": member.name,
+        "joined at": member.joined_at,
+        "server": member.guild.name, 
+        "isBot": member.bot
+        }
+        collection.insert_one(post)
+
+async def new_member(member):
+        """
+        Adds newly joined memebers while bot is on to the database
+        :param member: this is the memeber that has just joined
+        """
+        post = {
+            "_id": member.id, 
+            "name": member.name,
+            "joined at": member.joined_at,
+            "server": member.guild.name, 
+            "isBot": member.bot
+            }
+        collection.insert_one(post)
+
+async def remove_member(member):
+        """
+        Removes member from database when they leave the server
+        :param member: member that left
+        """
+        leaving_member = { "_id": member.id}
+        collection.delete_one(leaving_member)
+
+
